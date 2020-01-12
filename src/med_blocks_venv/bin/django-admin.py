6d0c1a9d9be65971ec649b3877abe35ba916e274
anmolcool007 <anmol.cool.gupta1999@gmail.com>
2020-01-12 07:29:59 +0530

    homepage

---
+++ src/med_blocks_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/anmolcool007/Desktop/MedBlocks/MedBlocks/src/med_blocks_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
